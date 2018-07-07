37830ab38818752c6a6026af8d3f16befa026876
Sandoche ADITTANE <sandoche@protonmail.com>
2018-07-07 16:34:25 +0200

    Update amp_generate.rb

--- lib/jekyll/amp_generate.rb
+++ lib/jekyll/amp_generate.rb
@@ -31,7 +31,7 @@ module Jekyll
     priority :low
     def generate(site)
       dir = site.config['ampdir'] || 'amp'
-      site.posts.docs.each do |post|
+      site.pages.docs.each do |post|
         next if post.data['skip_amp'] == true
         site.pages << AmpPost.new(site, site.source, File.join(dir, post.id), post)
       end
